4dc5f170c1fa1c5f121cd5947be8871b4026f9d7
Gleomit <draychev95@mail.ru>
2015-08-11 19:22:20 +0300

    --

---
+++ Homework - Transact-SQL/Problem 9. Define a .NET aggregate function/StrConcatImplementation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+/// <summary>
+/// Calculates the product of numerical values
+/// </summary>
+[System.Serializable]
+[Microsoft.SqlServer.Server.SqlUserDefinedAggregate(
+    Microsoft.SqlServer.Server.Format.Native,
+    IsInvariantToDuplicates = false, // receiving the same value again 
+				    // changes the result
+    IsInvariantToNulls = false,      // receiving a NULL value changes the result
+    IsInvariantToOrder = true,       // the order of the values doesn't 
+				    // affect the result
+    IsNullIfEmpty = true,            // if no values are given the result is null
+    Name = "StrConcat"                 // name of the aggregate
+
+)]
+
+
+public struct StrConcat
+{
+    // count only the vowels in the passed-in strings 
+    private List<SqlString> strings;
+
+    public void Init()
+    {
+        strings = new List<SqlString>();
+    }
+
+
+    public void Accumulate(SqlString value)
+    {
+        strings.Add(value);
+    }
+
+
+    public void Merge(StrConcat value)
+    {
+        Accumulate(value.Terminate());
+    }
+
+
+    public SqlString Terminate()
+    {
+        return string.Join(";", strings);
+    }
+}
